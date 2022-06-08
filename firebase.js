c5d7a1b63d81b56e17ed0edf9f3409ea9f21d826
CalebBarnes13 <94274053+CalebBarnes13@users.noreply.github.com>
2022-06-07 21:39:28 -0400

    firebase config

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOkTw5EuO-p3FKGG56cFRx0q1SKE0Lgog",
+  authDomain: "mediumclone-9149e.firebaseapp.com",
+  projectId: "mediumclone-9149e",
+  storageBucket: "mediumclone-9149e.appspot.com",
+  messagingSenderId: "73908814058",
+  appId: "1:73908814058:web:b51c0005465aa3a0a91793",
+  measurementId: "G-ZESJ10CQD5"
+};
